5dceb7009cfbc74a4fe1249f3099753cb04a91ce
Ajmal <mail@ajm.al>
2014-11-08 14:41:50 +0530

    pred

--- assets/javascripts/chitika_units.js
+++ assets/javascripts/chitika_units.js
@@ -1,14 +1,7 @@
 var AdUnitComponent = Ember.Component.extend({
   didInsertElement: function() {
-    // (function(){
-    //   var chitika_js = document.createElement('script');
-    //      chitika_js.type = 'text/javascript';
-    //      chitika_js.async = true;
-    //      chitika_js.src = '//cdn.chitika.net/getads.js';
-    //   (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(chitika_js);
-    // })();
     var rand = Math.floor((Math.random() * 1000) + 1);
-    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
+    window.CHITIKA = { 'units' : [] };
     var unit = {"calltype":"async[2]","publisher":"forumautomation","width":728,"height":90,"sid":"Chitika Default"};
     placement_id = window.CHITIKA.units.length;
     block_id = "chitikaAdBlock-"+placement_id;
