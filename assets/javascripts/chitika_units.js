c90f04a93ef79072a1958fc1b784b64e34e6be3f
Ajmal <mail@ajm.al>
2014-11-09 16:29:04 +0530

    chitika 0.1

--- assets/javascripts/chitika_units.js
+++ assets/javascripts/chitika_units.js
@@ -1,19 +1,14 @@
 var AdUnitComponent = Ember.Component.extend({
   didInsertElement: function() {
 
-    ( function() {
-
     var rand = Math.floor((Math.random() * 1000) + 1);
-
     if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
     var unit = {"calltype":"async[2]","publisher":"forumautomation","width":728,"height":90,"sid":"Chitika Default"};
     placement_id = window.CHITIKA.units.length;
     block_id = "chitikaAdBlock-"+placement_id;
     window.CHITIKA.units.push(unit);
-    this.$().html('<script type="text/javascript" src="//cdn.chitika.net/getads.js?r='+rand+'" async></script>'+
-      "<div id='" + block_id + "' class='chitika'></div>");
-
-    }());
+    this.$().html('<script type="text/javascript" src="//cdn.chitika.net/getads.js" async />'+"<div id='" + block_id + "' class='chitika'></div>");
+    
   }
 });
 
@@ -58,7 +53,8 @@ var AfterUserAboutAdMixin = AdMixinFactory(function(self, adUnit) {
    self.$(".user-main .about").after(adUnit.$()); 
   };
 });
-Discourse.SelectedPostsView.reopen(PrependAdMixin);
+
+Discourse.SelectedPostsView.reopen(BeforeAdMixin);
 Discourse.DiscoveryTopicsView.reopen(PrependAdMixin);
 Discourse.TopicFooterButtonsView.reopen(AfterAdMixin);
 Discourse.UserView.reopen(AfterUserAboutAdMixin);
