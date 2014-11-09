46f08bc9bb1057a0d0c71efa3acddb7599a439bf
Ajmal <mail@ajm.al>
2014-11-09 17:37:56 +0530

    chitika 0.1

--- assets/javascripts/chitika_units.js
+++ assets/javascripts/chitika_units.js
@@ -1,14 +1,20 @@
 var AdUnitComponent = Ember.Component.extend({
   didInsertElement: function() {
 
-    var rand = Math.floor((Math.random() * 1000) + 1);
     if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
     var unit = {"calltype":"async[2]","publisher":"forumautomation","width":728,"height":90,"sid":"Chitika Default"};
     placement_id = window.CHITIKA.units.length;
     block_id = "chitikaAdBlock-"+placement_id;
     window.CHITIKA.units.push(unit);
-    this.$().html('<script type="text/javascript" src="//cdn.chitika.net/getads.js" async />'+"<div id='" + block_id + "' class='chitika'></div>");
+    this.$().html("<div id='" + block_id + "' class='chitika'></div>");
     
+      var rand = Math.floor((Math.random() * 1000) + 1);
+      var chitika_js = document.createElement('script');
+      chitika_js.type = 'text/javascript';
+      chitika_js.async = true;
+      chitika_js.src = '//cdn.chitika.net/getads.js?ra='+rand;
+      this.$().append(chitika_js);
+
   }
 });
 
